5f8ec299c30db99f3aef7247c92b5f1522e4af51
Ross Jennings <rossjjennings@gmail.com>
2023-08-24 15:25:22 -0400

    BasebandModel and BasebandData in one file

--- cycspec_simulator/__init__.py
+++ cycspec_simulator/__init__.py
@@ -1,6 +1,5 @@
 from .template_profile import TemplateProfile
-from .baseband_model import BasebandModel
-from .baseband_data import BasebandData
+from .baseband import BasebandModel, BasebandData
 from .phase_predictor import FreqOnlyPredictor
 from .interpolation import fft_roll, fft_interp, lerp
 from .cycspec import outer_correlate, pspec_ryan4, pspec_corrfirst, pspec_numba
--- cycspec_simulator/baseband.py
+++ cycspec_simulator/baseband.py
@@ -1,4 +1,6 @@
 import numpy as np
+import numba as nb
+from numba.experimental import jitclass
 from .interpolation import fft_interp, lerp
 from .baseband_data import BasebandData
 
@@ -91,3 +93,25 @@ class BasebandModel:
         """
         n_samples = np.int64(duration*self.bandwidth)
         return sample(n_samples, phase_start, interp)
+
+@jitclass([
+    ('t', nb.float64[:]),
+    ('X', nb.complex128[:]),
+    ('Y', nb.complex128[:]),
+    ('L', nb.complex128[:]),
+    ('R', nb.complex128[:]),
+])
+class BasebandData:
+    feed_poln: str
+
+    def __init__(self, t, A, B, feed_poln):
+        self.t = t
+        self.feed_poln = feed_poln
+        if feed_poln.upper() == 'LIN':
+            self.X = A
+            self.Y = B
+        elif feed_poln.upper() == 'CIRC':
+            self.L = A
+            self.R = B
+        else:
+            raise ValueError(f"Invalid polarization type '{feed_poln}'.")
