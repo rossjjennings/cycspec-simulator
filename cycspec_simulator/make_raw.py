e3889a6b20e4254f90ca3000c2268e1a1760b13e
Ross Jennings <rossjjennings@gmail.com>
2023-09-07 14:40:22 -0400

    Standalone script to make raw files

---
+++ cycspec_simulator/make_raw.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+from .template_profile import TemplateProfile
+from .baseband import BasebandModel
+from .phase_predictor import FreqOnlyPredictor, PolynomialPredictor
+from .scattering import ExponentialScatteringModel
+from .time import Time
+from .metadata import ObservingMetadata
+from .guppi_raw import write
+
+def make_raw(template_file, polyco_file, out_file, n_samples, bandwidth,
+             obsfreq, scattering_time, start_mjd, start_second):
+    """
+    Create a file with simulated data in GUPPI Raw format.
+
+    Parameters
+    ----------
+    template_file: Name of PSRFITS file containing template profile
+    polyco_file: Name of TEMPO polyco file containing phase predictor
+    out_file: Name of file to write output to
+    n_samples: Number of samples to simulate
+    bandwidth: Channel bandwidth of simulated data (Hz)
+    obsfreq: Observing frequency of simulated data (Hz)
+    scattering_time: Scattering time used for ISM model
+    start_mjd: MJD of data start (if `None`, epoch from polyco file will be used)
+    start_second: UTC second of data start (if `None`, epoch from polyco file will be used)
+    """
+    template = TemplateProfile.from_file(template_file)
+    template.normalize()
+    template.make_posdef()
+
+    predictor = PolynomialPredictor.from_file(polyco_file)
+    model = BasebandModel(
+        template, bandwidth=bandwidth, predictor=predictor, obsfreq=obsfreq,
+    )
+    scattering_model = ExponentialScatteringModel(
+        scattering_time=scattering_time, bandwidth=model.bandwidth, cutoff=20,
+    )
+    pattern = scattering_model.realize()
+
+    if start_mjd is None:
+        start_mjd = predictor.epoch.mjd
+    if start_second is None:
+        start_second = predictor.epoch.second
+        start_second += int(np.round(predictor.epoch.offset))
+    t_start = Time(start_mjd, start_second, -(pattern.n_samples-1)/bandwidth)
+    data = model.sample(n_samples + pattern.n_samples - 1, t_start=t_start)
+    data = pattern.scatter(data)
+
+    metadata = ObservingMetadata.from_file(template_file)
+    metadata.observer = "cycspec-simulator"
+    write(out_file, data, metadata=metadata)
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--template-file', type=str, help="Template file (PSRFITS format)")
+    parser.add_argument('-p', '--polyco-file', type=str, help="TEMPO Polyco file")
+    parser.add_argument('-o', '--out-file', type=str, help="Output file name")
+    parser.add_argument('-n', '--n-samples', type=int, help="Number of samples to simulate")
+    parser.add_argument('-b', '--bandwidth', type=float, help="Channel bandwidth (MHz)")
+    parser.add_argument('-f', '--obsfreq', type=float, help="Observing frequency (MHz)")
+    parser.add_argument('-s', '--scattering-time', type=float, help="Scattering time (s)")
+    parser.add_argument('-m', '--start-mjd', type=int, default=None, help="MJD of data start")
+    parser.add_argument('-z', '--start-second', type=int, default=None, help="UTC second of data start")
+    args = parser.parse_args()
+
+    make_raw(args.template_file, args.polyco_file, args.out_file, args.n_samples, args.bandwidth*1e6,
+             args.obsfreq*1e6, args.scattering_time, args.start_mjd, args.start_second)
+
+if __name__ == "__main__":
+    main()
