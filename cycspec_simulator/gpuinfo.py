7b78f0812650d44b587159d06bbc5aa630bb3973
Ross Jennings <rossjjennings@gmail.com>
2023-10-26 17:31:09 -0400

    Reorganize gpuinfo

--- cycspec_simulator/gpuinfo.py
+++ cycspec_simulator/gpuinfo.py
@@ -52,8 +52,9 @@ def print_device_info():
             raise CudaError(err)
         clock_rate = get_cuda_attribute('CLOCK_RATE', device_id)
         memory_clock_rate = get_cuda_attribute('MEMORY_CLOCK_RATE', device_id)
+        memory_speed_gbps = 2 * memory_clock_rate # double data rate
         bus_width = get_cuda_attribute('GLOBAL_MEMORY_BUS_WIDTH', device_id)
-        memory_bandwidth = memory_clock_rate * 2 * bus_width / 8 # DDR, then bits -> bytes
+        memory_bandwidth = memory_speed_gbps * bus_width / 8 # bits -> bytes
         cc_major = get_cuda_attribute('COMPUTE_CAPABILITY_MAJOR', device_id)
         cc_minor = get_cuda_attribute('COMPUTE_CAPABILITY_MINOR', device_id)
         n_smp = get_cuda_attribute('MULTIPROCESSOR_COUNT', device_id)
@@ -66,22 +67,24 @@ def print_device_info():
                 raise
         max_threads_per_block = get_cuda_attribute('MAX_THREADS_PER_BLOCK', device_id)
         max_threads_per_smp = get_cuda_attribute('MAX_THREADS_PER_MULTIPROCESSOR', device_id)
+        max_total_threads = max_threads_per_smp*n_smp
         max_sharedmem_per_block = get_cuda_attribute('MAX_SHARED_MEMORY_PER_BLOCK', device_id)
         max_sharedmem_per_smp = get_cuda_attribute('MAX_SHARED_MEMORY_PER_MULTIPROCESSOR', device_id)
         print()
         print(f"Device {device_id}:")
         print(f"  Name: {device_name}")
-        print(f"  Total global memory: {device_total_mem/2**30:g} GiB")
-        print(f"  Maximum clock speed: {clock_rate/1e6:g} GHz")
-        print(f"  Maximum effective memory clock: {memory_clock_rate/1e6:g} GHz")
-        print(f"  Global memory bus width: {bus_width} bits")
-        print(f"  Memory bandwidth: {memory_bandwidth/1e6:g} GB/s")
         print(f"  Compute capability: {cc_major}.{cc_minor}")
+        print(f"  Total device memory: {device_total_mem/2**30:g} GiB")
+        print(f"  Device memory speed: {memory_speed_gbps/1e6:g} Gb/s")
+        print(f"  Device memory bus width: {bus_width} bits")
+        print(f"  Device memory bandwidth: {memory_bandwidth/1e6:g} GB/s")
+        print(f"  Maximum clock speed: {clock_rate/1e6:g} GHz")
         print(f"  Number of multiprocessors: {n_smp}")
-        if has_maxblocks:
-            print(f"  Max. blocks per multiprocessor: {max_blocks_per_smp}")
         print(f"  Max. threads per block: {max_threads_per_block}")
         print(f"  Max. threads per multiprocessor: {max_threads_per_smp}")
+        if has_maxblocks:
+            print(f"  Max. blocks per multiprocessor: {max_blocks_per_smp}")
+        print(f"  Max. total in-flight threads: {max_total_threads}")
         print(f"  Max. shared memory per block: {max_sharedmem_per_block/1024:g} kiB")
         print(f"  Max. shared memory per multiprocessor: {max_sharedmem_per_smp/1024:g} kiB")
 
