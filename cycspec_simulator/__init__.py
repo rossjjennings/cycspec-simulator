0394be13c667d2c6826415cf5fe235509ef7ae16
Ross Jennings <rossjjennings@gmail.com>
2023-11-14 10:14:10 -0500

    Make CUDA features optional

--- cycspec_simulator/__init__.py
+++ cycspec_simulator/__init__.py
@@ -4,10 +4,17 @@ from .phase_predictor import FreqOnlyPredictor, PolynomialPredictor
 from .scattering import ExponentialScatteringModel
 from .interpolation import fft_roll, fft_interp, lerp
 from .cycspec import cycfold_cpu
-from .cycspec_gpu import cycfold_gpu, cycfold_gpu_sharedmem
 from .folding import fold
 from .time import Time
 from .metadata import ObservingMetadata
+
+try:
+    import cupy
+    have_cuda = True
+except ImportError:
+    have_cuda = False
+if have_cuda:
+    from .cycspec_gpu import cycfold_gpu, cycfold_gpu_sharedmem
 
 from . import _version
 __version__ = _version.get_versions()['version']
