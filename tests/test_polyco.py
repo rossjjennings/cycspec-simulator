0e03533b81f47d8c2711ea808b12199cfaac1638
Ross Jennings <rossjjennings@gmail.com>
2026-06-04 17:41:03 -0400

    More polyco tests

--- tests/test_polyco.py
+++ tests/test_polyco.py
@@ -1,4 +1,5 @@
 import numpy as np
+import dask.array as da
 import pytest
 import os.path
 
@@ -26,3 +27,19 @@ def test_polyco_phase(polyco):
     phase_30min = polyco.phase(Time(60000, 1800, 0.))
     assert np.abs(phase_epoch - -1155516.122674652) <= 1e-8
     assert np.abs(phase_30min - 0.7849380372137075) <= 1e-8
+
+def test_polyco_supports_array_time(polyco):
+    offsets = np.linspace(0, 1800, 1025, endpoint=True)
+    phase = polyco.phase(Time(60000, 0, offsets))
+    assert phase.shape == (1025,)
+    assert np.abs(phase[0] - -1155516.122674652) <= 1e-8
+    assert np.abs(phase[-1] - 0.7849380372137075) <= 1e-8
+
+def test_polyco_supports_dask(polyco):
+    offsets = da.linspace(0, 1800, 1025, endpoint=True)
+    phase = polyco.phase(Time(60000, 0, offsets))
+    assert isinstance(phase, da.Array)
+    assert phase.shape == (1025,)
+    phase.compute()
+    assert np.abs(phase[0] - -1155516.122674652) <= 1e-8
+    assert np.abs(phase[-1] - 0.7849380372137075) <= 1e-8
